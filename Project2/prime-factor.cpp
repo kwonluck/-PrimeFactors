c33fea5ab992917464c97ccd7403e9bcfed0b7e3
kwonluck <software_k@naver.com>
2024-05-30 13:42:47 +0900

    Prime Factor 기능 구현 및 test code

--- Project2/prime-factor.cpp
+++ Project2/prime-factor.cpp
@@ -1,7 +1,22 @@
+#include <vector>
+using namespace std;
 
 
 class PrimeFactor {
 public:
 
+	vector<int> of(int number) {
+		vector<int> result = {};
+			int divisor = 2;
+			for (divisor = 2; number > 1; divisor++)
+			{
+				while (number % divisor == 0)
+				{
+					result.push_back(divisor);
+					number /= divisor;
+				}
+			}
+		return result;
+	}
 
 };
--- Sample-Test2/prime-factor_test.cpp
+++ Sample-Test2/prime-factor_test.cpp
@@ -1,9 +1,51 @@
 #include "pch.h"
 #include "../Project2/prime-factor.cpp"
+#include <vector>
+using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
+class PrimeFactorFixture : public testing::Test {
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+
+
+TEST_F(PrimeFactorFixture, Of1) {
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFactorFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFactorFixture, Of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFactorFixture, Of4) {
+	expected = { 2,2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
+
+TEST_F(PrimeFactorFixture, Of6) {
+	expected = { 2,3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+
+TEST_F(PrimeFactorFixture, Of9) {
+	expected = { 3,3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
+
+TEST_F(PrimeFactorFixture, Of12) {
+	expected = { 2,2,3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
+
+TEST_F(PrimeFactorFixture, Of15) {
+	expected = { 3,5 };
+	EXPECT_EQ(expected, prime_factor.of(15));
 }
